a104e23a8254ac95a8d3efb7461aa96aca337a1d
DariaSviatenko <svyatikdasha@gmail.com>
2022-10-18 17:58:40 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,6 +6,8 @@ const itemsMarkup = createGalleryItems(galleryItems);
 galleryContainer.insertAdjacentHTML("beforeend", itemsMarkup);
 galleryContainer.addEventListener("click", onGalleryContainerClick);
 
+let instance = null;
+
 function createGalleryItems(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
@@ -22,26 +24,29 @@ function createGalleryItems(galleryItems) {
     })
     .join("");
 }
+
 function onGalleryContainerClick(evt) {
+  evt.preventDefault();
   const isGalleryImageEl = evt.target.classList.contains("gallery__image");
   if (!isGalleryImageEl) {
     return;
   }
-  evt.preventDafault();
+  instance = basicLightbox.create(`
+  <div class="modal">
+      <img src="${evt.target.dataset.source}" width="1200" height="800">
+      </div>`);
+
+  instance.show();
 }
 
 function onEscKeyPress(evt) {
   const ESC_KEY_CODE = "Escape";
-  if (evt.code === ESC_KEY_CODE) {
-    onCloseModal();
+  const isEscKey = evt.code === ESC_KEY_CODE;
+  if (isEscKey && instance) {
+
+    instance.close();
   }
 }
-const instance = basicLightbox.create(`
-    <div class="modal">
-        <p>
-        </p>
-    </div>
-`);
+document.addEventListener("keydown", onEscKeyPress);
 
-instance.show();
-console.log(galleryItems);
+
